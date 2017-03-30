2c29e086b5dba464d103a58ee6f7ebef88881e62
Carlos <urco13@gmail.com>
2017-03-30 10:59:13 +0200

    Add files via upload

---
+++ 3EvaluacionFutbol/src/pkg3evaluacionfutbol/Fubol.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3evaluacionfutbol;
+
+/**
+ *
+ * @author dam115
+ */
+public class Fubol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Futbolista futbolista1 = new Futbolista("Carlos", 6, Demarcacion.DELANTERO, Equipo.BARÇA);
+    }
+    
+}
---
+++ 3EvaluacionFutbol/src/pkg3evaluacionfutbol/Futbolista.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3evaluacionfutbol;
+
+/**
+ *
+ * @author dam115
+ */
+
+public class Futbolista {
+    	private int dorsal;
+	private String Nombre;
+	private Demarcacion demarcacion;
+	private Equipo equipo;
+
+	public Futbolista() {
+	}
+
+	public Futbolista(String nombre, int dorsal, Demarcacion demarcacion, Equipo equipo) {
+		this.dorsal = dorsal;
+		Nombre = nombre;
+		this.demarcacion = demarcacion;
+		this.equipo = equipo;
+	}
+
+	// Metodos getter y setter
+	@Override
+	public String toString() {
+		return this.dorsal + " - " + this.Nombre + " - "
+				+ this.demarcacion.name() + " - " + this.equipo.getNombreClub();
+	}
+}
